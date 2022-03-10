ca692632c459996486f09259b6c7c5c8826479eb
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-03-10 00:00:00 -0300

    Rotas

---
+++ autenticacao/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cadastro/', views.cadastro, name="cadastro"),
+    path('logar/', views.logar, name="logar"),
+]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def cadastro(request):
+    return HttpResponse('Cadastro')
+
+def logar(request):
+    return  HttpResponse('Logar')
