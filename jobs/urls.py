b874ad6945eef4bb4665ba96480e1934485e786e
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-04-02 00:00:00 -0300

    Edita dados do perfil

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('encontrar_jobs/', views.encontrar_jobs, name="encontrar_jobs"),
     path('aceitar_job/<int:id>/', views.aceitar_job, name="aceitar_job"),
+    path('perfil/', views.perfil, name="perfil"),
 ]
