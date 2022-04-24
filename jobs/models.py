bd39ba264ab463bab18f9854aa28527f0667262a
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-04-24 19:00:00 -0300

    CMD: (env) python manage.py makemigrations

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Referencias(models.Model):
+    arquivo = models.FileField(upload_to='referencias')
+
+    def __str__(self) -> str:
+        return self.arquivo.url
+
+
+class Jobs(models.Model):
+    categoria_choices = (('D', 'Design'),
+                         ('EV', 'Edição de Vídeo'))
+    status_choices = (('C', 'Em criação'),
+                      ('AA', 'Aguardando aprovação'),
+                      ('F', 'Finalizado'))
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    categoria = models.CharField(max_length=2, choices=categoria_choices, default="D")
+    prazo_entrega = models.DateTimeField()
+    preco = models.FloatField()
+    referencias = models.ManyToManyField(Referencias)
+    profissional = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
+    reservado = models.BooleanField(default=False)
+    status = models.CharField(max_length=2, default='AA')
+
+    def __str__(self) -> str:
+        return self.titulo
