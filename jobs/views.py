978ba086f40d8e1b9517c6d9f6beda8b37b2a501
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-04-29 19:00:00 -0300

    Carrega 'jobs' do banco de dados

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from jobs.models import Jobs
+
 
 def encontrar_jobs(request):
-    return render(request, 'encontrar_jobs.html')
+    if request.method == "GET":
+        jobs = Jobs.objects.filter(reservado=False)
+        return render(request, 'encontrar_jobs.html', {'jobs': jobs})
