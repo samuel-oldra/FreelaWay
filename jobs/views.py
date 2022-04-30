1c030c964946425241f77dae5654b831674f99ef
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-04-30 19:00:00 -0300

    Filtra e carrega 'jobs' do banco de dados

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 
 from jobs.models import Jobs
@@ -5,5 +6,40 @@ from jobs.models import Jobs
 
 def encontrar_jobs(request):
     if request.method == "GET":
-        jobs = Jobs.objects.filter(reservado=False)
+        preco_minimo = request.GET.get('preco_minimo')
+        preco_maximo = request.GET.get('preco_maximo')
+
+        prazo_minimo = request.GET.get('prazo_minimo')
+        prazo_maximo = request.GET.get('prazo_maximo')
+
+        categoria = request.GET.get('categoria')
+
+        if preco_minimo or preco_maximo or prazo_minimo or prazo_maximo or categoria:
+            if not preco_minimo:
+                preco_minimo = 0
+            if not preco_maximo:
+                preco_maximo = 999999
+
+            if not prazo_minimo:
+                prazo_minimo = datetime(year=1900, month=1, day=1)
+            if not prazo_maximo:
+                prazo_maximo = datetime(year=3000, month=1, day=1)
+
+            if categoria == '':
+                categoria = ['D', 'EV', ]
+            elif categoria == 'D':
+                categoria = ['D', ]
+            elif categoria == "EV":
+                categoria = ['EV', ]
+
+            jobs = Jobs.objects \
+                .filter(preco__gte=preco_minimo) \
+                .filter(preco__lte=preco_maximo) \
+                .filter(prazo_entrega__gte=prazo_minimo) \
+                .filter(prazo_entrega__lte=prazo_maximo) \
+                .filter(categoria__in=categoria) \
+                .filter(reservado=False)
+        else:
+            jobs = Jobs.objects.filter(reservado=False)
+
         return render(request, 'encontrar_jobs.html', {'jobs': jobs})
